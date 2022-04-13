2329da3deea3778d20781da4a2d1333095c969db
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-04-13 15:13:42 +0500

    Update projectdata.js

--- js/projectdata.js
+++ js/projectdata.js
@@ -1,4 +1,4 @@
-export default dataProjects = [{
+export default dataProjects=[{
     projectTitle: 'Project name goes here',
     popupLanguages: ['HTML/CSS', 'Ruby on Rails', 'Javascript', 'React'],
     imgShow: ['./images/proj1.png', './images/proj1.png', './images/proj1.png', './images/proj1.png'],
@@ -45,4 +45,4 @@ export default dataProjects = [{
     pText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.<br>Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     liveUrl: 'https://fatima-hub333.github.io/Website_Mobile_Version_Repeated/',
     sourceUrl: 'https://github.com/Fatima-hub333/Website_Mobile_Version_Repeated/tree/main'
-  }]
+  }];
